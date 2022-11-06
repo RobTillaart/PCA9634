0f8173c9e232d21212ce075dfca3df83226e1273
Luxovious <39154233+Luxovious@users.noreply.github.com>
2022-11-06 20:20:35 +0100

    added writeStop

--- PCA9634.h
+++ PCA9634.h
@@ -98,8 +98,11 @@ public:
   // generic worker, write N consecutive PWM registers
   uint8_t  writeN(uint8_t channel, uint8_t* arr, uint8_t count);
 
-    // generic worker, write N consecutive PWM registers without Stop command
+  // generic worker, write N consecutive PWM registers without Stop command
   uint8_t  writeN_noStop(uint8_t channel, uint8_t* arr, uint8_t count);
+
+  // write stop command to end transmission
+  uint8_t  writeStop();
   
   // reg = 1, 2  check datasheet for values
   uint8_t  writeMode(uint8_t reg, uint8_t value);
