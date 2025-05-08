c4e148ba3f18eb600fa6d531a3495c1800bff997
Rob Tillaart <rob.tillaart@gmail.com>
2025-05-08 13:52:41 +0200

    remove old defines

--- PCA9634.h
+++ PCA9634.h
@@ -16,43 +16,26 @@
 
 
 //  mode codes
-//  NEW
 #define PCA963X_MODE1               0x00
 #define PCA963X_MODE2               0x01
-//  OLD
-#define PCA9634_MODE1               0x00
-#define PCA9634_MODE2               0x01
 
 
 //  0x80 bit ==> Auto-Increment for all registers.
 //               used in writeN() - see issue #9
-//  NEW
 #define PCA963X_PWM(x)              (0x82+(x))
 #define PCA963X_GRPPWM              0x0A
 #define PCA963X_GRPFREQ             0x0B
-//  OLD
-#define PCA9634_PWM(x)              (0x82+(x))
-#define PCA9634_GRPPWM              0x0A
-#define PCA9634_GRPFREQ             0x0B
 
 
 //  check datasheet for details
-//  NEW
 #define PCA963X_LEDOUT_BASE         0x0C  //  0x0C..0x0D
 #define PCA963X_LEDOFF              0x00  //  default @ startup
 #define PCA963X_LEDON               0x01
 #define PCA963X_LEDPWM              0x02
 #define PCA963X_LEDGRPPWM           0x03
-//  OLD
-#define PCA9634_LEDOUT_BASE         0x0C  //  0x0C..0x0D
-#define PCA9634_LEDOFF              0x00  //  default @ startup
-#define PCA9634_LEDON               0x01
-#define PCA9634_LEDPWM              0x02
-#define PCA9634_LEDGRPPWM           0x03
 
 
 //  Error codes
-//  NEW
 #define PCA963X_OK                  0x00
 #define PCA963X_ERROR               0xFF
 #define PCA963X_ERR_WRITE           0xFE
@@ -60,18 +43,9 @@
 #define PCA963X_ERR_MODE            0xFC
 #define PCA963X_ERR_REG             0xFB
 #define PCA963X_ERR_I2C             0xFA
-//  OLD
-#define PCA9634_OK                  0x00
-#define PCA9634_ERROR               0xFF
-#define PCA9634_ERR_WRITE           0xFE
-#define PCA9634_ERR_CHAN            0xFD
-#define PCA9634_ERR_MODE            0xFC
-#define PCA9634_ERR_REG             0xFB
-#define PCA9634_ERR_I2C             0xFA
 
 
 //  Configuration bits MODE1 register
-//  NEW
 #define PCA963X_MODE1_AUTOINCR2     0x80  //  ReadOnly,  0 = disable  1 = enable
 #define PCA963X_MODE1_AUTOINCR1     0x40  //  ReadOnly,  bit1
 #define PCA963X_MODE1_AUTOINCR0     0x20  //  ReadOnly,  bit0
@@ -81,57 +55,28 @@
 #define PCA963X_MODE1_SUB3          0x02  //  0 = disable      1 = enable
 #define PCA963X_MODE1_ALLCALL       0x01  //  0 = disable      1 = enable
 #define PCA963X_MODE1_NONE          0x00
-//  OLD
-#define PCA9634_MODE1_AUTOINCR2     0x80  //  ReadOnly,  0 = disable  1 = enable
-#define PCA9634_MODE1_AUTOINCR1     0x40  //  ReadOnly,  bit1
-#define PCA9634_MODE1_AUTOINCR0     0x20  //  ReadOnly,  bit0
-#define PCA9634_MODE1_SLEEP         0x10  //  0 = normal       1 = sleep
-#define PCA9634_MODE1_SUB1          0x08  //  0 = disable      1 = enable
-#define PCA9634_MODE1_SUB2          0x04  //  0 = disable      1 = enable
-#define PCA9634_MODE1_SUB3          0x02  //  0 = disable      1 = enable
-#define PCA9634_MODE1_ALLCALL       0x01  //  0 = disable      1 = enable
-#define PCA9634_MODE1_NONE          0x00
 
 
 //  Configuration bits MODE2 register
-//  NEW
 #define PCA963X_MODE2_BLINK         0x20  //  0 = dim          1 = blink
 #define PCA963X_MODE2_INVERT        0x10  //  0 = normal       1 = inverted
 #define PCA963X_MODE2_ACK           0x08  //  0 = on STOP      1 = on ACK
 #define PCA963X_MODE2_TOTEMPOLE     0x04  //  0 = open drain   1 = totem-pole
 #define PCA963X_MODE2_NONE          0x00
-//  OLD
-#define PCA9634_MODE2_BLINK         0x20  //  0 = dim          1 = blink
-#define PCA9634_MODE2_INVERT        0x10  //  0 = normal       1 = inverted
-#define PCA9634_MODE2_ACK           0x08  //  0 = on STOP      1 = on ACK
-#define PCA9634_MODE2_TOTEMPOLE     0x04  //  0 = open drain   1 = totem-pole
-#define PCA9634_MODE2_NONE          0x00
 
 
 //  Registers in which the ALLCALL and sub-addresses are stored
-//  NEW
 #define PCA963X_SUBADR(x)           (0x0D +(x))  //  x = 1..3
 #define PCA963X_ALLCALLADR          0x11
-//  OLD
-#define PCA9634_SUBADR(x)           (0x0D +(x))  //  x = 1..3
-#define PCA9634_ALLCALLADR          0x11
 
 
 //  Standard ALLCALL and sub-addresses --> only work for write commands and NOT for read commands
-//  NEW
 #define PCA963X_ALLCALL             0x70  //  TDS of chip says 0xE0, however,
                                           //  in this library the LSB is added during the write command
                                           //                 (0xE0 --> 0b11100000, 0x70 --> 0b1110000)
 #define PCA963X_SUB1                0x71  //  see line above (0xE2 --> 0x71)
 #define PCA963X_SUB2                0x72  //  see line above (0xE4 --> 0x72)
 #define PCA963X_SUB3                0x74  //  see line above (0xE8 --> 0x74)
-//  OLD
-#define PCA9634_ALLCALL             0x70  //  TDS of chip says 0xE0, however,
-                                          //  in this library the LSB is added during the write command
-                                          //                 (0xE0 --> 0b11100000, 0x70 --> 0b1110000)
-#define PCA9634_SUB1                0x71  //  see line above (0xE2 --> 0x71)
-#define PCA9634_SUB2                0x72  //  see line above (0xE4 --> 0x72)
-#define PCA9634_SUB3                0x74  //  see line above (0xE8 --> 0x74)
 
 
 
